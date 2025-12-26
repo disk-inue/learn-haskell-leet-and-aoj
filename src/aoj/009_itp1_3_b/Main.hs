0c1bfe4c5f12b387fa7020ebf9a1410916d0e4ba
disk-inue <disk.inue.01@gmail.com>
2025-12-26 13:51:32 +0900

    feat : Day9

---
+++ src/aoj/009_itp1_3_b/Main.hs
@@ -0,0 +1,12 @@
+printCase :: Int -> Int -> IO ()
+printCase inputCount inputNum
+  | 0 == inputNum = pure ()
+  | otherwise = do
+      putStrLn $ "Case " ++ show inputCount ++ ": " ++ show inputNum
+      newInputNum <- readLn
+      printCase (inputCount + 1) newInputNum
+
+main :: IO ()
+main = do
+  inputNum <- readLn
+  printCase 1 inputNum
